e9ec4106bb5fce6d1084ed0ab10581a23fbeb852
Ilya <56665977+Ily-droid@users.noreply.github.com>
2022-06-16 23:53:59 +0300

    Add files via upload

---
+++ task.cpp
@@ -0,0 +1,31 @@
+﻿
+#include <iostream>
+#include <vector>
+#include <string>
+#include <filesystem>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+    string path = "/";
+    string fname;
+
+    ofstream fout;
+    fout.open(path);
+    char poshuk_array[100];
+    
+    
+
+    cout << "Enter name of file";
+    cin >> fname;
+
+    std::vector<std::string> strings = { "stuff", "things", "misc" };
+    for (auto& s : strings) {
+        std::cout << s << std::endl;
+    }
+   /* for (const auto& file : directory_iterator(path))
+        cout << file.path() << endl;*/
+
+    return 0;//EXIT_SUCCESS;
+}
